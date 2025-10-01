8b9bcdc1483096f2d30267196f7b5cbf35e005ba
Ittichet Thongsang <zagif2151382@gmail.com>
2025-10-01 21:00:45 +0700

    Gameover page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@
 import Image from "next/image";
 import Landing from "@/components/Landing";
 import HUD from "@/components/HUD";
+import GameOver from "@/components/GameOver";
+
 export default function Home() {
   return (
     <div className="flex font-suse items-center justify-items-center min-h-screen">
-      <HUD score={0} highScore={0} timeLeft={30} />
+      {/* <HUD score={0} highScore={0} timeLeft={30} /> */}
       {/* <Landing highScore={0} onStart={() => {}} /> */}
+      <GameOver score={0} highScore={0} onRestart={() => {}} />
     </div>
   );
 }
---
+++ src/components/GameOver.tsx
@@ -0,0 +1,27 @@
+type GameOverProps = {
+  score: number;
+  highScore: number;
+  onRestart: () => void;
+};
+
+export default function GameOver({
+  score,
+  highScore,
+  onRestart,
+}: GameOverProps) {
+  return (
+    <div className="flex flex-col gap-[1rem] w-[100%] justify-center items-center">
+      <div>
+        <h1 className="text-5xl font-black">Game Over</h1>
+      </div>
+      <p className="text-xl">Your Score: {score}</p>
+      <p className="text-xl">High Score: {highScore}</p>
+      <button
+        onClick={onRestart}
+        className="bg-amber-400 p-2 px-5 rounded-lg text-2xl lg:text-3xl hover:bg-amber-300 cursor-pointer hover:text-gray-700"
+      >
+        Play Again
+      </button>
+    </div>
+  );
+}
