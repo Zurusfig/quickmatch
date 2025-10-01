13bcaecf916c9fc718f1841744486858a9f53aff
Ittichet Thongsang <zagif2151382@gmail.com>
2025-10-01 20:37:26 +0700

    big fixes for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Image from "next/image";
-import { Landing } from "@/components/Landing";
+import Landing from "@/components/Landing";
 export default function Home() {
   return (
     <div className="flex font-suse items-center justify-items-center min-h-screen">
-      <Landing/>
+      <Landing highScore={0} onStart={() => {}} />
     </div>
   );
 }
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,14 +1,26 @@
-export function Landing(){
-    return <div className="flex flex-col gap-[1rem] w-[100%] justify-center items-center">
-        <div>
-            <h1 className="text-5xl font-black">QuickMatcH</h1>
-        </div>
-        <div className="text-2xl font">
-            HighScore: 
-        </div>
-        <button className="bg-amber-400 p-2 w-[20%] rounded-lg text-xl hover:bg-amber-300 cursor-pointer hover:text-gray-600">
-            Start
-        </button>
-        <p className="text-grey-700 text-sm">Find the matching symbols. Score as much as you can in 30s!</p>
-    </div> 
+"use client";
+
+type LandingProps = {
+  highScore: number;
+  onStart: () => void;
+};
+
+export default function Landing({ highScore, onStart }: LandingProps) {
+  return (
+    <div className="flex flex-col gap-[1rem] w-[100%] justify-center items-center">
+      <div>
+        <h1 className="text-5xl font-black">QuickMatcH</h1>
+      </div>
+      <div className="text-2xl font">High Score: {highScore}</div>
+      <button
+        onClick={onStart}
+        className="bg-amber-400 p-2 w-[20%] rounded-lg text-xl hover:bg-amber-300 cursor-pointer hover:text-gray-700"
+      >
+        Start
+      </button>
+      <p className="text-grey-700 text-sm">
+        Find the matching symbols. Score as much as you can in 30s!
+      </p>
+    </div>
+  );
 }
